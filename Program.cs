570b4b89d51ed7cf359b3fc92c30a115e04542f9
Daniel Scholl <daniel.scholl@microsoft.com>
2019-09-27 09:38:10 -0500

    Initial Code

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Security;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace iot_extractor
+{
+    class Program
+    {
+        private static string _idScope = string.Empty;
+        private static string _registrationId = Environment.MachineName.ToLower();
+
+        private const string GlobalDeviceEndpoint = "global.azure-devices-provisioning.net";
+
+        static int Main(string[] args)
+        {
+
+            if ((args.Length > 0))
+            {
+                _registrationId = args[0];
+
+                var r = new Regex("^[a-z0-9-]*$");
+                if (!r.IsMatch(_registrationId))
+                {
+                    throw new FormatException("Invalid registrationId: The registration ID is alphanumeric, lowercase, and may contain hyphens");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(_idScope) && (args.Length > 1))
+            {
+                _idScope = args[1];
+            }
+
+            using (var security = new SecurityProviderTpmHsm(_registrationId))
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("\tEndorsementKey:  ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("{0} ", Convert.ToBase64String(security.GetEndorsementKey()));
+                Console.WriteLine(Environment.NewLine);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("\tRegistrationId: :  ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("{0} ", security.GetRegistrationID());
+                Console.WriteLine(Environment.NewLine);
+
+                if (!String.IsNullOrEmpty(_idScope)) Register(security).Wait();
+
+            }
+            return 0;
+        }
+
+
+        static async Task Register(SecurityProviderTpmHsm security)
+        {
+            Console.WriteLine("Press ENTER when ready to execute Registration.");
+            Console.WriteLine("------------------");
+            Console.ReadLine();
+
+            using (var transport = new ProvisioningTransportHandlerAmqp(TransportFallbackType.TcpOnly))
+            {
+                var client = ProvisioningDeviceClient.Create(GlobalDeviceEndpoint, _idScope, security, transport);
+
+                try
+                {
+                    DeviceRegistrationResult result = await client.RegisterAsync().ConfigureAwait(false);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("\tAssigned Hub: :  ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("{0} ", result.AssignedHub);
+                    Console.WriteLine(Environment.NewLine);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("\tDeviceId: :  ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("{0} ", result.DeviceId);
+                    Console.WriteLine(Environment.NewLine);
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("The registration has failed");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+# tpm-extractor
+
+[![Build Status](https://dascholl.visualstudio.com/IoT/_apis/build/status/danielscholl.tpm-extractor?branchName=master)](https://dascholl.visualstudio.com/IoT/_build/latest?definitionId=50&branchName=master)
+
+Retrieve a TPM 2.0 Enrollment Key to be used with the Azure Device Provisioning Service.
+
+## Usage
+
+Pass <RegistrationId> (optional) to override the `hostname` as the default RegistrationId
+Pass <RegistrationId> and <IdScope> to execute DPS Provisioning.
+
+Usage: [sudo] dotnet tpm.dll \<RegistrationId\> \<IdScope\> 
+
+*Note*: Run this 'As Administrator' (Windows Powershell) or 'SUDO' (Linux)
+
+## Execution
+
+1. Run this application
+2. Retrieve an Endorsement key for your device
+3. Switch to the Azure Portal
+4. In Azure Device Provisioning Service under 'Manage enrollments' select 'Individual Enrollments'
+5. Select 'Add individual enrollment' and fill in:
+    1. Mechanism 'TPM'
+    2. Endorsement key
+    3. Registration Id
+6. Save the individual enrollment
+
+
+```powershell
+$RELEASE="20190927.1"
+$ID_SCOPE="<your_idscope>"
+$REGISTRATION="edge-gateway"
+
+. {Invoke-WebRequest -useb https://github.com/danielscholl/tpm-extractor/releases/download/$RELEASE/build.zip -Outfile tpm-extractor.zip}
+Expand-Archive -Path ".\tpm-extractor.zip"
+dotnet run tpm-extractor\tpm.dll $ID_SCOPE $REGISTRATION
+
+. {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
+Install-SecurityDaemon -Dps -ContainerOs Linux -ScopeId $ID_SCOPE -RegistrationId $REGISTRATION_ID
+```
+
+## Exceptions
+
+If you get this 'TbsCommandBlocked' execption during the execution of the application:
+
+    Unhandled Exception: Microsoft.Azure.Devices.Provisioning.Client.ProvisioningTransportException: AMQP transport exception ---> Tpm2Lib.TpmException: Error {TbsCommandBlocked} was returned for command ActivateCredential.
+
+Check if you are running it as Adminsitrator or 'SU'.
+
